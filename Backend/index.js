5bce0ae67eba052be7bb0d4ff0f531e87934c6f8
Shahwaz Alam <shahwaz575@gmail.com>
2026-02-26 05:45:18 +0530

    url updated

--- Backend/index.js
+++ Backend/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://stays-hub-property-rental-website.vercel.app",
   credentials: true,
 }));
 
